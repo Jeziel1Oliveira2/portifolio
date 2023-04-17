f637de878110f6c9ac095264702017fa5f440659
Jeziel <85239929+Jeziel1Oliveira2@users.noreply.github.com>
2023-04-17 09:59:09 -0300

    mudado

---
+++ src/header/index.jsx
@@ -0,0 +1,24 @@
+export function Header() {
+    return (
+        <header>
+            <div className="father">
+                <div className="brothers">
+                    Ínicio
+                </div>
+                <div className="brothers">
+                    Sobre mim
+                </div>
+                <div className="brothers">
+                    Projetos
+                </div>
+                <div className="brothers">
+                    Curriculo
+                </div>
+                <div className="brothers">
+                    Meios de contato
+                </div>
+            </div>
+        </header>
+    )
+    
+}
